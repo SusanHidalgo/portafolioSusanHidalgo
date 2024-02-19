15eceb826fe8549fe9dc3ea9ae624662f83d89b9
SusanHidalgo <shidalgo70985@ufide.ac.cr>
2024-02-18 21:14:34 -0600

    Parte del ProyectConfig

--- Tienda_SusanHidalgo/src/main/java/Tienda/demo/ProyectConfig.java
+++ Tienda_SusanHidalgo/src/main/java/Tienda/demo/ProyectConfig.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Tienda.demo;
+package tienda.demo;
 
-/**
- *
- * @author User
- */
-public class ProyectConfig {
+import java.util.Locale;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+
+public class ProyectConfig implements WebMvcConfigurer{
+  
+     @Bean
+    public SessionLocaleResolver localeResolver() {
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.getDefault());
+        slr.setLocaleAttributeName("session.current.locale");
+        slr.setTimeZoneAttributeName("session.current.timezone");
+       
+        return slr;
+    }
+   
+    @Bean
+    public LocaleChangeInterceptor localeChanceInterceptor(){
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+   
+    @Override
+    public void addInterceptors(InterceptorRegistry registro){
+        registro.addInterceptor(localeChanceInterceptor());
+    }
+   
+    @Bean("messageBrouce")
+    public MessageSource messageSource(){
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
     
 }
