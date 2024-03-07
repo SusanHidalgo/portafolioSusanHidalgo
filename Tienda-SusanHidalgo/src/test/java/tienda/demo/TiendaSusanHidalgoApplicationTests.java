dbb3a7d505eed81db55fc491aa270fbe03fd76ab
SusanHidalgo <shidalgo70985@ufide.ac.cr>
2024-03-06 19:17:39 -0600

    Portafolio Corregido desde 0

---
+++ Tienda-SusanHidalgo/src/test/java/tienda/demo/TiendaSusanHidalgoApplicationTests.java
@@ -0,0 +1,13 @@
+package tienda.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaSusanHidalgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
