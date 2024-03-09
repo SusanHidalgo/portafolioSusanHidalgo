a3ebc98879b9840135a70fc435fe991f1d4c0ba6
SusanHidalgo <shidalgo70985@ufide.ac.cr>
2024-03-08 21:17:31 -0600

    Revert "Tienda Solucionada"
    This reverts commit 29ed917d609da7d475e761a7412c3a08420af982.

---
+++ Tienda-SusanHidalgo/src/main/java/tienda/demo/service/CategoriaService.java
@@ -0,0 +1,11 @@
+package tienda.demo.service;
+
+import tienda.demo.domain.Categoria;
+import java.util.List;
+
+public interface CategoriaService {
+    
+    
+    public List<Categoria> getCategorias(boolean activos);
+ 
+}
